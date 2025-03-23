478a257646e5e828fd8be752f937b1f22e4e8a21
Paul Kamma <paul.kamma@gmail.com>
2025-03-23 20:22:47 +0100

    Added get_live_matchmap for Match model

--- csgomatches/models.py
+++ csgomatches/models.py
@@ -214,6 +214,12 @@ class Match(models.Model):
     def get_first_matchmap(self) -> 'MatchMap | None':
         return self.matchmap_set.order_by('starting_at').first()
 
+    def get_live_matchmap(self) -> 'MatchMap | None':
+        for mmap in self.matchmap_set.order_by('map_nr'):
+            if mmap.is_live():
+                return mmap
+            return False
+
     def is_live(self) -> bool | None:
         if self.has_ended():
             return False
